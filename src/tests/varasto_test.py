f00fd15cb838afc2a043ee6bce8b6e5b15b3e42f
VP-MaxHax <vp.juselius@gmail.com>
2023-10-30 22:23:57 +0200

    Varasto test coverage 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,60 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_kaikki_mita_voidaan(self):
+        self.varasto.lisaa_varastoon(4)
+
+        saatu_maara = self.varasto.ota_varastosta(8)
+
+        self.assertEqual(saatu_maara, 4)
+
+    def test_varastoon_ei_voida_laittaa_liikaa(self):
+        self.varasto.lisaa_varastoon(15)
+
+        self.assertEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_varaston_tyhjennys_ei_vie_saldoa_negatiiviseksi(self):
+        self.varasto.lisaa_varastoon(4)
+
+        self.varasto.ota_varastosta(8)
+
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_varastoo_ei_voi_lisata_negatiivista_maaraa(self):
+
+        self.varasto.lisaa_varastoon(-4)
+
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_varaston_saldon_oikea_tulostus_muoto(self):
+        self.varasto.lisaa_varastoon(3)
+
+        self.assertEqual(str(self.varasto), f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}")
+
+    def test_varaston_koko_nolla(self):
+        varasto = Varasto(0)
+
+        self.assertEqual(varasto.tilavuus, 0.0)
+
+    def test_ei_voi_ottaa_tyhjasta_varastosta(self):
+        self.varasto.ota_varastosta(5)
+
+        self.assertEqual(self.varasto.saldo, 0.0)
+
+    def test_alku_saldo_negatiivinen(self):
+        varasto = Varasto(10, -5)
+
+        self.assertEqual(varasto.saldo, 0.0)
+
+    def test_alku_saldo_yli_maksimin(self):
+        varasto = Varasto(10, 15)
+
+        self.assertEqual(varasto.saldo, 10)
+
+    def test_otetaan_negatiivisesta_saldosta(self):
+        self.varasto.lisaa_varastoon(5)
+
+        saatu_maara = self.varasto.ota_varastosta(-5)
+
+        self.assertEqual(saatu_maara, 0.0)        
